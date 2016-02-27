664e60eff42a34d7b5f44735792eac6c8318b64d
Suui <suui.gd@gmail.com>
2016-02-27 02:12:39 +0100

    Green: have_2_points_for_a_parranda

--- Tests/HandShould.cs
+++ Tests/HandShould.cs
@@ -28,6 +28,15 @@ namespace Tests
 			var hand = new Hand(first, second, third);
 			hand.Points.Should().Be(1);
 		}
+
+		[Test]
+		public void have_2_points_for_a_parranda()
+		{
+			var hand = new Hand(new Card(Suit.Clubs, Value.Five),
+								new Card(Suit.Spades, Value.Five),
+								new Card(Suit.Hearts, Value.Five));
+			hand.Points.Should().Be(2);
+		}
 	}
 
 	public class Hand
@@ -35,7 +44,7 @@ namespace Tests
 		private Card First { get; }
 		private Card Second { get; }
 		private Card Third { get; }
-		public int Points => 1;
+		public int Points => First.Equals(Second) && Second.Equals(Third) ? 2 : 1;
 
 		public Hand(Card first, Card second, Card third)
 		{
